bdc2940c3471f34e4171a583505193c6acef0542
Skywing Wang <sgtwan17@liverpool.ac.uk>
2021-11-03 16:46:12 +0000

    Create template.R

---
+++ template.R
@@ -0,0 +1,47 @@
+
+
+
+
+getOrders <- function(store,newRowList,currentPos,info,params) {
+  
+  allzero  <- rep(0,length(newRowList)) 
+  
+  if (is.null(store)) {
+    checkParams(params)
+    store <- initStore(newRowList,params$series)
+  }
+  
+  store <- updateStore(store, newRowList, params$series)
+  
+  
+  
+  
+  
+  
+  
+  return(list(store=store,marketOrders=marketOrders,
+              limitOrders1=allzero,
+              limitPrices1=allzero,
+              limitOrders2=allzero,
+              limitPrices2=allzero))
+}
+
+initClStore  <- function(newRowList,series) {
+  clStore <- matrix(0,nrow=maxRows,ncol=length(series))
+  return(clStore)
+}
+updateClStore <- function(clStore, newRowList, series, iter) {
+  for (i in 1:length(series))
+    clStore[iter,i] <- as.numeric(newRowList[[series[i]]]$Close)
+  return(clStore)
+}
+initStore <- function(newRowList,series) {
+  count <- vector(mode="numeric",length=length(series)) # stores # of days in trade
+  return(list(iter=0,cl=initClStore(newRowList,series),count=count))
+}
+updateStore <- function(store, newRowList, series) {
+  store$iter <- store$iter + 1
+  store$cl <- updateClStore(store$cl,newRowList,series,store$iter) 
+  return(store)
+}
+
