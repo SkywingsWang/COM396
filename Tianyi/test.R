b5706a1380641c56fa5ff7e83290c9a78f53dc08
Skywing Wang <sgtwan17@liverpool.ac.uk>
2021-11-05 00:14:51 +0000

    latest

---
+++ Tianyi/test.R
@@ -0,0 +1,65 @@
+# The strategy will be long (short) whenever 
+
+maxRows <- 3100
+
+cciOverSold <- -100
+
+getOrders <- function(store,newRowList,currentPos,info,params) {
+  
+  allzero  <- rep(0,length(newRowList)) 
+  
+  if (is.null(store)) store <- initStore(newRowList,params$series)
+  store <- updateStore(store, newRowList, params$series)
+  
+  marketOrders <- -currentPos; pos <- allzero
+  
+  if (store$iter > params$lookback) {
+    
+    startIndex <-  store$iter - params$lookback
+    
+    for (i in 1:length(params$series)) {
+      
+      cl <- newRowList[[params$series[i]]]$Close
+      cci <- last(CCI(store$cl[startIndex:store$iter,i],
+                            n=params$lookback,c=params$cciMeanDev))
+      
+      macd <- last(MACD(store$cl[startIndex:store$iter,i],
+                      nFast=params$macdFast, nSlow=params$macdSlow,
+                      maType=params$macdMa, percent=TRUE))
+      
+      if (cci < cciOverSold && !is.na(cci)) {
+        # if the cci value is below -100, we take long position
+        pos[params$series[i]] <- 1
+      }
+      else if (macd[,"signal"] > macd[,"macd"]) {
+        pos[params$series[i]] <- -1
+      }
+    }
+  }
+  
+  marketOrders <- marketOrders + pos
+  
+  return(list(store=store,marketOrders=marketOrders,
+              limitOrders1=allzero,
+              limitPrices1=allzero,
+              limitOrders2=allzero,
+              limitPrices2=allzero))
+}
+
+initClStore  <- function(newRowList,series) {
+  clStore <- matrix(0,nrow=maxRows,ncol=length(series))
+  return(clStore)
+}
+updateClStore <- function(clStore, newRowList, series, iter) {
+  for (i in 1:length(series))
+    clStore[iter,i] <- as.numeric(newRowList[[series[i]]]$Close)
+  return(clStore)
+}
+initStore <- function(newRowList,series) {
+  return(list(iter=0,cl=initClStore(newRowList,series)))
+}
+updateStore <- function(store, newRowList, series) {
+  store$iter <- store$iter + 1
+  store$cl <- updateClStore(store$cl,newRowList,series,store$iter) 
+  return(store)
+}
