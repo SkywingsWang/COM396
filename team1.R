0bca0c43ef0f8599378cf6e4c998f3ec3428eca5
Drpipi <78703298+Drpipi@users.noreply.github.com>
2022-03-22 20:05:00 +0000

    Update team1.R

--- team1.R
+++ team1.R
@@ -4,7 +4,9 @@ params <- list(series=1:10,
                BBLookback=50,BBsd=5,
                kdjLookback=20,nFastK=14,nFastD=3,
                nSlowD=5,Jhigh=0.8,Jlow=0.2,
-               DCLookback=19,maLookback=3)
+               DCLookback=19,maLookback=3,
+               momentumLookback=90, momentumTestlength=225,
+               momentumHolddays=45)
 
 maxRows <- 11000
 
@@ -36,7 +38,10 @@ getOrders <- function(store,newRowList,currentPos,info,params) {
   dcLastTran <- store$dcPos
   dcCoefficient <- 50000
   
-  if(store$iter > params$DCLookback && store$iter <= 225) {
+  #We use correlation coefficient got from momentum strategy to put constraint on DonchianChannel strategy
+  #During the first 225(=momentumTestlength) days, momentum strategy is collecting data and 
+  #won't work. So only DonchianChannel strategy and cci-based strategy will work during this time
+  if(store$iter > params$DCLookback && store$iter <= params$momentumTestlength) {
     
     startIndex <-  store$iter - params$DCLookback
     maxCl <- 0
@@ -100,14 +105,29 @@ getOrders <- function(store,newRowList,currentPos,info,params) {
     }
   }
   
-  if(store$iter > 225) {
+  #The following if statement contains the combination of momentum strategy and DonchianChannel strategy
+  #Momentum strategy and DonchianChannel strategy won't work at the same time
+  #Use the correlation test to determine whether apply momentum strategy or DonchianChannel strategy
+  #Momentum strategy is a long-term trend following strategy
+  #DonchianChannel strategy is a mean reversion strategy
+  
+  #If the series pass the correlation test, apply the momentum strategy, 
+  #otherwise, apply DonchianChannel strategy
+  #The main idea is that if the series passes the correlation test, it indicates that 
+  #the following 30 days are expected to follow the past 225(=momentumTestlength) day's trend
+  #So probably, DonchianChannel strategy will not catch the low point in this period 
+  if(store$iter > params$momentumTestlength) {
     
+    testlength <- params$momentumTestlength
+    lookback <- params$momentumLookback
+    holddays <- params$momentumHolddays
     maxCl <- 0
     
     for (i in 1:length(params$series)){
       maxCl <- max(maxCl,newRowList[[params$series[i]]]$Close)
     }
     
+    #To get the correlation coefficient got from last correlatio test
     corr <- store$cor
     
     for (i in 1:length(params$series)) {
@@ -116,7 +136,10 @@ getOrders <- function(store,newRowList,currentPos,info,params) {
       startDay = 30*(store$iter%/%30)-90
       endDay = 30*(store$iter%/%30)
       
+      #every 30 days, the momentum strategy will do a correlation test and decide whether to long or to short
       if(store$iter %% 30 == 1) {
+        #The following if stratement is used to stop the momentum strategy's position
+        #if the series passed correlation test last time and the position kept being held in the past 30 days
         if(corr[params$series[i]] > 0.2){
           if(momentumLastTran[params$series[i]] != 0) {
             momentumPos[params$series[i]] <- -momentumLastTran[params$series[i]]
@@ -124,32 +147,52 @@ getOrders <- function(store,newRowList,currentPos,info,params) {
           }
         }
         corr[params$series[i]] = 0
+        #initialize two parameters used for the correlation test
         lookback_return <- c()
         holddays_return <- c()
         
+        #lookback return = the close price of one day of series i- the close price 45(=lookback) days ago
+        #holddays return = the return we get if we hold 1 position of series i for 90(=holddays) days
+        #right after we get the lookback return
+        #In the past 225(=testlength) days, get 90(testlength-lookback-holddays) pairs of lookback return
+        #and corresponding holddays return
         for (k in 1:90) {
-          lookback_return <- c(lookback_return, store$cl[store$iter+k-(225-90+2),i]-store$cl[store$iter+k-225-1,i])
-          holddays_return <- c(holddays_return, store$cl[store$iter+k-90-2,i]- store$cl[store$iter+k-(225-90+1),i])
+          lookback_return <- c(lookback_return, store$cl[store$iter+k-(testlength-lookback+2),i]-store$cl[store$iter+k-testlength-1,i])
+          holddays_return <- c(holddays_return, store$cl[store$iter+k-lookback-2,i]- store$cl[store$iter+k-(testlength-lookback+1),i])
         }
         
+        #Apply cor() function to get to correlation coefficient of the lookback return and holddays return
+        #A correlation coefficient bigger than 0.2 indicates a strong trend in the past 225 days
         corr[params$series[i]] = cor(lookback_return,holddays_return)
         
+        #When we apply the momentum strategy, we have to consider a special situation where 
+        #though we caught a trend in the past 225 days, the trend stoped a few days ago,
+        #in this case, stop loss will be triggered right after we hold a position.
+        #We try to aviod this situation by comparing cl price of series i and EMA(store$cl[store$iter-89:store$iter,i], 90)[91]
         if(cor(lookback_return,holddays_return)>=0.2) {
           if(i != 3){
-            if (store$cl[store$iter-1,i] - store$cl[store$iter-90,i] > 0) {
+            #If the series i pass the correlation test, 
+            #long if the close price of series i is bigger than the cl price of series i 90 days ago
+            if (store$cl[store$iter-1,i] - store$cl[store$iter-90,i] > 0 && store$cl[store$iter,i] >= EMA(store$cl[store$iter-89:store$iter,i], 90)[91]) {
               limitPrice[params$series[i]] <-newRowList[[params$series[i]]]$Close
               limitPos[params$series[i]] <- 100000 %/% newRowList[[params$series[i]]]$Close
+              #record the position of the transaction which will be stored in store
               momentumLastTran[params$series[i]] <- limitPos[params$series[i]]
             }
-            # if the past lookback period saw a negative return, short
-            else {
+            #If the series i pass the correlation test, 
+            #short if the close price of series i is smaller than the cl price of series i 90 days ago
+            else if(store$cl[store$iter-1,i] - store$cl[store$iter-90,i] < 0 && store$cl[store$iter,i] <= EMA(store$cl[store$iter-89:store$iter,i], 90)[91]) {
               limitPrice[params$series[i]] <- newRowList[[params$series[i]]]$Close
               limitPos[params$series[i]] <- -(100000 %/% newRowList[[params$series[i]]]$Close)
+              #record the position of the transaction which will be stored in store
               momentumLastTran[params$series[i]] <- limitPos[params$series[i]]
             }
           }
         }
       }
+      
+      #If the series i failed in the correaltion test last time,
+      #apply DonchianChannel strategy in the following 30 days.
       else if(corr[i]< 0.2) {
         cl <- newRowList[[params$series[i]]]$Close
         Merge <- cbind(store$high[startIndex:store$iter,i],store$low[startIndex:store$iter,i],store$cl[startIndex:store$iter,i])
@@ -203,18 +246,32 @@ getOrders <- function(store,newRowList,currentPos,info,params) {
           dcLastTran[params$series[i]] <- 0
         }
       }
+      
+      #If the series i passed the correaltion test last time,momentum strategy was applied
+      #test whether to stop loss or not every day in the following 30 days
       else if(corr[i]>=0.2) {
+        #Limit order may fail, so if the series i passed the correlation test,
+        #at the next day, test if the limit order succeeds.
         if(store$iter %% 30 == 2){
+          #if the position we stored in store is bigger than 0, we long last day
+          #If today's low price of series i is bigger than the cl price last day, limit orders fail
+          #So set the recorded position to zero in store
           if(momentumLastTran[params$series[i]] > 0){
             if(newRowList[[params$series[i]]]$Low > store$cl[store$iter-1,params$series[i]]){
               momentumLastTran[params$series[i]] <- 0
             }
-          }else if(momentumLastTran[params$series[i]] < 0){
+          }
+          #if the position we stored in store is smaller than 0, we short last day
+          #If today's low price of series i is smaller than the cl price last day, limit orders fail
+          #set the recorded position to zero in store
+          else if(momentumLastTran[params$series[i]] < 0){
             if(newRowList[[params$series[i]]]$High < store$cl[store$iter-1,params$series[i]]){
               momentumLastTran[params$series[i]] <- 0
             }
           }
         }
+        
+        #The following if and else if statement is momentum strategy's stop loss and stop win.
         if(momentumLastTran[params$series[i]] > 0 && store$cl[endDay,i] >= EMA(store$cl[startDay:endDay,i], 90)[91]){
           if (newRowList[[params$series[i]]]$Close <= EMA(store$cl[startDay:endDay,i], 90)[91]) {
             momentumPos[params$series[i]] <- -(100000 %/% store$cl[endDay+1,params$series[i]])
@@ -224,14 +281,6 @@ getOrders <- function(store,newRowList,currentPos,info,params) {
             momentumLastTran[params$series[i]] <- 0
           }
         }
-        
-        else if (momentumLastTran[params$series[i]] > 0) {
-          if(newRowList[[params$series[i]]]$Close >= 1.09 * store$cl[endDay,i]){
-            momentumPos[params$series[i]] <- -(100000 %/% store$cl[endDay+1,params$series[i]])
-            momentumLastTran[params$series[i]] <- 0
-          }
-        }
-        
         else if (momentumLastTran[params$series[i]] < 0 && store$cl[endDay,i] <= EMA(store$cl[startDay:endDay,i], 90)[91]) {
           if (newRowList[[params$series[i]]]$Close >= EMA(store$cl[startDay:endDay,i], 90)[91]) {
             momentumPos[params$series[i]] <- 100000 %/% store$cl[endDay+1,params$series[i]]
@@ -241,17 +290,11 @@ getOrders <- function(store,newRowList,currentPos,info,params) {
             momentumLastTran[params$series[i]] <- 0
           }
         }
-        
-        else if (momentumLastTran[params$series[i]] < 0) {
-          if(newRowList[[params$series[i]]]$Close <= 0.91 * store$cl[endDay,i]){
-            momentumPos[params$series[i]] <- 100000 %/% store$cl[endDay+1,params$series[i]]
-            momentumLastTran[params$series[i]] <- 0
-          }
-        }
       }
     }
     
     limitOrders <- limitOrders + limitPos
+    #Update the correlation coefficient in store got from the correlation test if new test is committed.
     store <- updateCorr(store,corr)
   }
   
@@ -265,9 +308,9 @@ getOrders <- function(store,newRowList,currentPos,info,params) {
   # the cci value and the set benchmark (cciOverSold and cciOverBought).
   # Stop loss: BBands() function. If touched, then clean the position.
   if (store$iter > params$cciLookback) {
-
+    
     startIndex <-  store$iter - params$cciLookback
-
+    
     # Get the maximum close price of all the time series, for position sizing.
     maxCl <- 0
     for (i in 1:length(params$series)){
@@ -356,7 +399,7 @@ getOrders <- function(store,newRowList,currentPos,info,params) {
         }
       }
     }
-    }
+  }
   
   # Store all the values we need
   store <- updateMomentumPos(store,momentumLastTran)
